374207512dca5b6676f17bbd98627e8a8a597074
OliverNi <Oliver.Nilsson@hotmail.se>
2014-12-17 12:56:19 +0100

    Changed class variables to private, added get/set

--- Tree/Node.java
+++ Tree/Node.java
@@ -4,8 +4,8 @@ package Tree;
  * Created by Oliver on 2014-12-16.
  */
 public abstract class Node {
-    Node parent;
-    String name;
+    private Node parent;
+    private String name;
     public Node() {
         this(null, "");
     }
@@ -18,4 +18,19 @@ public abstract class Node {
         return -1;
     }
 
+    public Node getParent() {
+        return parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
