c69874af5d783c8dceb1d3a05ea96a5ed262be81
Oliver Nilsson <Oliver.Nilsson@hotmail.se>
2015-01-11 18:04:11 +0100

    Fixed copy so name can be changed

--- Tree/FileTree.java
+++ Tree/FileTree.java
@@ -114,7 +114,7 @@ public class FileTree implements Serializable {
      * @param destinationPath path to the destination including node name.
      * @return true if successful.
      */
-    public boolean copyNode(String[] sourcePath, String[] destinationPath, BlockDevice mBlockDevice){
+    public boolean copyNode(String[] sourcePath, String[] destinationPath, BlockDevice mBlockDevice, boolean source){
         Node sourceNode = getNode(currentDir, sourcePath);
         Node destinationNode = getNode(currentDir, HelpFunctions.removeLast(destinationPath));
         if (sourceNode != null && destinationNode != null && destinationNode instanceof Folder){
@@ -124,12 +124,15 @@ public class FileTree implements Serializable {
                 }
             }
             else if (sourceNode instanceof Folder){
+                if (source) {
+                    destinationPath = HelpFunctions.addElement(destinationPath, sourcePath[sourcePath.length - 1]);
+                }
                 createDirectory(destinationPath);
 
                 for (Map.Entry<String, Node> entry : ((Folder) sourceNode).getChildren().entrySet()){
                     String[] childSourcePath = HelpFunctions.addElement(sourcePath, entry.getKey());
                     String[] childDestinationPath = HelpFunctions.addElement(destinationPath, entry.getKey());
-                    copyNode(childSourcePath,  childDestinationPath, mBlockDevice);
+                    copyNode(childSourcePath,  childDestinationPath, mBlockDevice, false);
                 }
                 return true;
             }
